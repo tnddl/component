b978bd4bef8f5fb2389fb8a85036333028988694
tnddl <tnddl3434@naver.com>
2023-07-30 17:31:37 +0900

    core components

---
+++ App.js
@@ -0,0 +1,104 @@
+import { StatusBar } from 'expo-status-bar';
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Image source={require("./dandelion.jpg")} style={styles.local_image}/>
+      <Image
+        source={{
+          uri: "http://img.khan.co.kr/news/2019/11/29/l_2019112901003607500286631.jpg",
+        }}
+        style={styles.url_image}
+      />
+
+      <TextInput placeholder="이름을 입력해주세요" />
+
+      <Button
+        title="Click Me!"
+        onPress={() => {
+          console.log("clicked!");
+        }}
+      />
+
+      <Switch value={true} />
+      <Switch value={false} />
+
+      <ScrollView>
+        <Image
+          source={require("./dandelion.jpg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./dandelion.jpg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./dandelion.jpg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./dandelion.jpg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./dandelion.jpg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./dandelion.jpg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./dandelion.jpg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./dandelion.jpg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./dandelion.jpg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./dandelion.jpg")}
+          style={styles.local_image}
+        />
+      </ScrollView>
+    
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  local_image: {
+    width: 100,
+    height: 100,
+  },
+  url_image: {
+    width: 200,
+    height: 200,
+  },
+});
